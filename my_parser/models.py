75a746ebf61888b212427c83a34f6a1fa4273403
Gennadii87 <Gennadii_s@inbox.ru>
2024-06-28 00:20:49 +1000

    update

--- my_parser/models.py
+++ my_parser/models.py
@@ -8,3 +8,6 @@ class File(models.Model):
     name = models.CharField(max_length=255)
     data_creation = models.DateTimeField(default=timezone.now)
     file = models.FileField(upload_to='image/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.file}"
