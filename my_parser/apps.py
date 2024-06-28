16bd383c1d0777f25b83b2c50de10eda9328f7b3
Gennadii87 <Gennadii_s@inbox.ru>
2024-06-29 00:14:13 +1000

    add signals

--- my_parser/apps.py
+++ my_parser/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MyParserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'my_parser'
+
+    def ready(self):
+        import my_parser.signals
